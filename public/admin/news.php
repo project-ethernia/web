94f5eaafaf90f412af5208399f846d48088bb5af
sx <scxrl666@gmail.com>
2025-11-23 17:05:16 +0100

    asdasdsad

--- public/admin/news.php
+++ public/admin/news.php
@@ -251,7 +251,7 @@ $news = $stmt->fetchAll();
   <title>ETHERNIA Admin - Hírek</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="/admin/assets/css/news.css?v=3">
+  <link rel="stylesheet" href="/admin/assets/css/news.css?v=4">
 </head>
 <body class="admin-body">
   <div class="admin-layout">
